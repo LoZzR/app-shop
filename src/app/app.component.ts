6fdbf8c9fc4c8f7c3cb8d0e20278f6e424260ea5
LoZzR <zakariaelhichem@gmail.com>
2020-07-12 15:35:17 +0100

    App creation

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+
+}
