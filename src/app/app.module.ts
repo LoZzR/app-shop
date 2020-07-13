572eeb37c11fa69298790ade897915785e26bc8a
LoZzR <zakariaelhichem@gmail.com>
2020-07-13 22:57:46 +0100

    add shop-list component wih a navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+
+import { ShopsListComponent } from './shops/shops-list/shops-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopsListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule],
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTabsModule],
   providers: [
 
   ],
